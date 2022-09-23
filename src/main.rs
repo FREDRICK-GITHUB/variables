0c9c2e9c94ccda766cadd860215422ac45875091
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-09-23 10:14:07 +0300

    floating point types example

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
 fn main() {
-    let  x= 5;  // initial value assigned to x is 5
+   let x = 2.0;
 
-    let x = x +1;  //we are reusing the variable name. The memory address is different for this value
+   let y: f32 = 3.0;
 
-    let x = x * 2; //yet another value of the same variable name. Even the type can change in shadowing
-
-    println!("The value of x is {}", x);
-
-    /* 
-    Perfect example of a case where shadowing is necessary:
-    let spaces = "    ";
-    let spaces = spaces.len(); 
-    data type has changed in this case
-    */
+   println!("The two numbers added here are x:{} and y:{}", x,y);
 }

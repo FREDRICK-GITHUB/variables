a21e8c2298135f472c47123b41a3f9290637eedb
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-09-23 08:23:47 +0300

    added comments on shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
-    let  x= 5;
+    let  x= 5;  // initial value assigned to x is 5
 
-    let x = x +1;
+    let x = x +1;  //we are reusing the variable name. The memory address is different for this value
 
-    let x = x * 2;
+    let x = x * 2; //yet another value of the same variable name. Even the type can change in shadowing
 
     println!("The value of x is {}", x);
+
+    /* 
+    Perfect example of a case where shadowing is necessary:
+    let spaces = "    ";
+    let spaces = spaces.len(); 
+    data type has changed in this case
+    */
 }

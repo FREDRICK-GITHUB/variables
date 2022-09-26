fcd53cde01483890372a03ba13d72242f4140c39
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-09-26 08:16:28 +0300

    added boolean and character types

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,15 @@ fn main() {
    let remainder = 43 % 5;
 
    println!("sum: {} difference: {} product: {} quotient: {} remainder: {}", sum, difference, product, quotient, remainder);
+
+   let t = true;
+   let f: bool = false;
+
+   println!("the bolean is: {} and explicitly declared boolean is: {}",t, f);
+
+   let c = 'z';
+   let z = 'Z';
+   let heart_eyed_cat = '😻';
+
+   println!("characters are declared with a single quote in Rust. c: {}, z: {}, heart_eyed_cat: {}", c, z, heart_eyed_cat);
 }
